69e761871009dfd36b6afe9e826818e18614eb54
Archit <arch10@ymail.com>
2020-11-25 02:56:15 +0530

    minor fixes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,9 @@ import logo from "../../assets/icons/logo.svg";
 function Header() {
     return (
         <div className="header">
-            <img src={logo} alt="Covid Tracker" className="header__icon" />
+            <a href="/">
+                <img src={logo} alt="Covid Tracker" className="header__icon" />
+            </a>
             <h3 className="header__title">Covid Tracker</h3>
         </div>
     );
